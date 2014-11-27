d817643791c1e857bb242bf996b812d9ec15fd78
jamaga <hipo.beaver@gmail.com>
2014-11-27 18:35:41 +0100

    changed name of the app, GET and POST are working with curl

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20141127153833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "benders", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "messages", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
